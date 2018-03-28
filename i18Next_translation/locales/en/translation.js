78e44c63242a6456a5067eac719ba18dc2ed1a91
Zahir Firasta <zahir.firasta@gmail.com>
2018-03-28 16:44:17 +0530

    Translation fire for English

---
+++ i18Next_translation/locales/en/translation.js
@@ -0,0 +1,12 @@
+{
+	"labels": {
+		"selectLanguages": "Select language",
+		"language1": "English",
+		"language2": "German",
+        "sampleText": "Selected language is English"
+	},
+	"messages": {
+		"alreadySelected": "Already selected!",
+        "languageChanged": "Language changed."
+	}
+}
