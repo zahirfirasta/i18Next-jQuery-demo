8b782559d3749a5985476a491663deb321261143
Zahir Firasta <zahir.firasta@gmail.com>
2018-03-28 16:46:42 +0530

    Create translation.js

---
+++ i18Next_translation/locales/de/translation.js
@@ -0,0 +1,12 @@
+{
+	"labels": {
+		"selectLanguages": "Sprache auswählen",
+		"language1": "Englisch",
+		"language2": "Deutsche",
+        "sampleText": "Die ausgewählte Sprache ist Deutsch"
+	},
+	"messages": {
+		"alreadySelected": "Bereits ausgewählt!",
+        "languageChanged": "Sprache geändert."
+	}
+}
